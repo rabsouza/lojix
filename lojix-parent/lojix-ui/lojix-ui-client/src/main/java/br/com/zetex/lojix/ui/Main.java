b741c35d319e436190eb1abc282230b8ab13a240
Rafael Batista <rabsouza@gmail.com>
2013-03-12 23:00:05 -0300

    Add project lojix parent e ui

---
+++ lojix-parent/lojix-ui/lojix-ui-client/src/main/java/br/com/zetex/lojix/ui/Main.java
@@ -0,0 +1,12 @@
+package br.com.zetex.lojix.ui;
+
+/**
+ * Enterprise Application Client main class.
+ *
+ */
+public class Main {
+    
+    public static void main( String[] args ) {
+        System.out.println( "Hello World Enterprise Application Client!" );
+    }
+}
